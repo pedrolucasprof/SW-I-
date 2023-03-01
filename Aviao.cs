85b5c7a6641648e64e166646924c500ac4519234
Pedro Lucas <100379713+pedrolucasprof@users.noreply.github.com>
2023-03-01 16:59:24 -0300

    Add files via upload

---
+++ Aviao.cs
@@ -0,0 +1,12 @@
+namespace exemplo_1;
+class Aviao
+{
+    public string modelo;
+    public int velocidade;
+    public int altitude;
+    public string marca; 
+
+    public void subir(){
+        altitude++;
+    }
+
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿namespace exemplo_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
