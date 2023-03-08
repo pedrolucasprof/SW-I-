bc1d70afcdac7e804b76922532f45c4777b06d22
Pedro Lucas <100379713+pedrolucasprof@users.noreply.github.com>
2023-03-08 16:54:09 -0300

    Add files via upload

---
+++ exercicio/Exemplo.cs
@@ -0,0 +1,38 @@
+namespace exercicio;
+class Exemplo
+{
+     //ATRIBUTOS
+    public string nome="";
+
+    public int idade=0;
+
+    public bool vacinado=false;
+
+//MÉTODOS
+
+    public void mostramsg(){
+       Console.WriteLine("Oiie 2F");
+    }
+
+   public void peganome(string texto){
+    Console.WriteLine("Oieee" + texto);
+   }
+
+    public string mensagem(){
+        return "Devolvendo um texto";
+    }
+
+    public int somar(int a , int b){
+        return a+b;
+    }
+
+}
+    
+    
+
+
+
+
+
+
+
---
+++ exercicio/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exercicio;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+    
+    // Console.ReadKey();
+
+    //Criar uma instancia (objeto) a partir de uma Classe
+    Exemplo exe01 = new Exemplo();
+
+   
+
+     exe01.mostramsg();
+     exe01.peganome("Pedro");
+     Console.WriteLine(exe01.mensagem());
+     Console.WriteLine("O resultado da soma é" + exe01.somar(10,20));
+     
+       }
+        
+}
